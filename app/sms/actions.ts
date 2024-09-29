347139e5fd88d9e0fa1a466ebeb3f428e3d4b2fb
jaemin <als982001@naver.com>
2024-09-29 17:08:52 +0900

    sms validation 수정

--- app/sms/actions.ts
+++ app/sms/actions.ts
@@ -1,10 +1,41 @@
+import { redirect } from "next/navigation";
 import validator from "validator";
 import { z } from "zod";
 
-const phoneSchema = z.string().trim().refine(validator.isMobilePhone);
+const phoneSchema = z
+  .string()
+  .trim()
+  .refine(
+    (phone) => validator.isMobilePhone(phone, "ko-KR"),
+    "Wrong phone format"
+  );
 const tokenSchema = z.coerce.number().min(100000).max(999999);
 
-export async function smsLogin(prevState: any, formData: FormData) {
-  console.log(formData.get("token"));
-  console.log(tokenSchema.parse(formData.get("token")));
+interface ActionState {
+  token: boolean;
+}
+
+export async function smsLogin(prevState: ActionState, formData: FormData) {
+  const phone = formData.get("phone");
+  const token = formData.get("token");
+
+  if (!prevState.token) {
+    const result = phoneSchema.safeParse(phone);
+
+    if (!result.success) {
+      return { token: false, error: result.error.flatten() };
+    }
+
+    return {
+      token: true,
+    };
+  }
+
+  const result = tokenSchema.safeParse(token);
+
+  if (!result.success) {
+    return { token: true, error: result.error.flatten() };
+  }
+
+  redirect("/");
 }
--- app/sms/page.tsx
+++ app/sms/page.tsx
@@ -5,8 +5,13 @@ import Button from "@/components/Button";
 import { useFormState } from "react-dom";
 import { smsLogin } from "./actions";
 
+const initialState = {
+  token: false,
+  error: undefined,
+};
+
 export default function SMSLogin() {
-  const [status, dispatch] = useFormState(smsLogin, null);
+  const [state, dispatch] = useFormState(smsLogin, initialState);
 
   return (
     <div className="flex flex-col gap-10 py-8 px-6">
@@ -15,16 +20,24 @@ export default function SMSLogin() {
         <h2 className="text-xl">Verify your phone number.</h2>
       </div>
       <form action={dispatch} className="flex flex-col gap-3">
-        <Input name="phone" type="number" placeholder="Phone number" required />
-        <Input
-          name="token"
-          type="number"
-          placeholder="Verification code"
-          required
-          min={100000}
-          max={999999}
-        />
-        <Button text="Verify" />
+        {state.token ? (
+          <Input
+            name="token"
+            type="number"
+            placeholder="Verification code"
+            required
+            min={100000}
+            max={999999}
+          />
+        ) : (
+          <Input
+            name="phone"
+            type="number"
+            placeholder="Phone number"
+            required
+          />
+        )}
+        <Button text={state.token ? "Verify Token" : "Send Verification SMS"} />
       </form>
     </div>
   );
