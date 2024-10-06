3c0c4c6e37da4e5b4d05d25d7c776c23b9108229
jaemin <als982001@naver.com>
2024-10-06 15:07:31 +0900

    로그인/아웃 상태에 따른 미들웨어 적용

--- middleware.ts
+++ middleware.ts
@@ -1,14 +1,40 @@
 import { NextRequest, NextResponse } from "next/server";
 import getSession from "./lib/session";
 
+interface IRouts {
+  [key: string]: boolean;
+}
+
+const publicOnlyUrls: IRouts = {
+  "/": true,
+  "/login": true,
+  "/sms": true,
+  "/create-account": true,
+};
+
 export async function middleware(request: NextRequest) {
   const session = await getSession();
 
-  console.log(session);
+  const exists = publicOnlyUrls[request.nextUrl.pathname];
 
-  if (request.nextUrl.pathname === "/profile") {
-    return NextResponse.redirect(new URL("/", request.url));
+  // 로그아웃 상태
+  if (!session.id) {
+    // 로그아웃 상태에서 못 가는 곳으로 진입하는 경우
+    if (!exists) {
+      return NextResponse.redirect(new URL("/", request.url));
+    }
+
+    return NextResponse.next();
   }
+
+  // 로그인을 한 상태
+
+  // 로그인을 한 상황에서 로그인 페이지로 진입하는 경우A
+  if (exists) {
+    return NextResponse.redirect(new URL("/products", request.url));
+  }
+
+  return NextResponse.next();
 }
 
 export const config = {
