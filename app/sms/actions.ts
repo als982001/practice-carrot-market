3d97576d57ec2f961478a1f259c74346135dd2f9
jaemin <als982001@naver.com>
2024-10-19 16:11:28 +0900

    twilio 적용(주석처리함)

--- app/sms/actions.ts
+++ app/sms/actions.ts
@@ -1,20 +1,27 @@
 "use server";
 
-import crypto from "crypto";
 import { redirect } from "next/navigation";
+
+import crypto from "crypto";
 import validator from "validator";
+import twilio from "twilio";
 import { z } from "zod";
 
 import db from "@/lib/db";
+import { setUserSession } from "@/utils/authUtils";
 
-const phoneSchema = z
-  .string()
-  .trim()
-  .refine(
-    (phone) => validator.isMobilePhone(phone, "ko-KR"),
-    "Wrong phone format"
-  );
-const tokenSchema = z.coerce.number().min(100000).max(999999);
+async function tokenExists(token: number) {
+  const exists = await db.sMSToken.findUnique({
+    where: {
+      token: token.toString(),
+    },
+    select: {
+      id: true,
+    },
+  });
+
+  return Boolean(exists);
+}
 
 async function getToken() {
   const token = crypto.randomInt(100000, 999999).toString();
@@ -32,6 +39,19 @@ async function getToken() {
 
   return token;
 }
+
+const phoneSchema = z
+  .string()
+  .trim()
+  .refine(
+    (phone) => validator.isMobilePhone(phone, "ko-KR"),
+    "Wrong phone format"
+  );
+const tokenSchema = z.coerce
+  .number()
+  .min(100000)
+  .max(999999)
+  .refine(tokenExists, "This token does not exists");
 
 interface ActionState {
   token: boolean;
@@ -77,18 +97,47 @@ export async function smsLogin(prevState: ActionState, formData: FormData) {
       },
     });
 
+    /*
     // send the token using twlio
+    const client = twilio(
+      process.env.TWILIO_ACCOUNT_SID,
+      process.env.TWILIO_AUTH_TOKEN
+    );
+
+    await client.messages.create({
+      body: `Your carrot verification code is: ${token}`,
+      from: process.env.TWILIO_PHONE_NUMBER!,
+      to: process.env.MY_PHONE_NUMBER!,
+    });
+    */
 
     return {
       token: true,
     };
   }
 
-  const result = tokenSchema.safeParse(token);
+  const result = await tokenSchema.spa(token);
 
   if (!result.success) {
     return { token: true, error: result.error.flatten() };
   }
 
-  redirect("/");
+  const tokenInfo = await db.sMSToken.findUnique({
+    where: {
+      token: result.data.toString(),
+    },
+    select: {
+      id: true,
+      userId: true,
+    },
+  });
+
+  await setUserSession(tokenInfo!.userId);
+  await db.sMSToken.delete({
+    where: {
+      id: tokenInfo!.id,
+    },
+  });
+
+  redirect("/profile");
 }
