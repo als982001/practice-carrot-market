02b6aa5e07a8e7934888f77ebb155b3c25880886
jaemin <als982001@naver.com>
2024-10-19 14:54:21 +0900

    twlio 이용 준비

--- app/sms/actions.ts
+++ app/sms/actions.ts
@@ -1,6 +1,11 @@
+"use server";
+
+import crypto from "crypto";
 import { redirect } from "next/navigation";
 import validator from "validator";
 import { z } from "zod";
+
+import db from "@/lib/db";
 
 const phoneSchema = z
   .string()
@@ -10,6 +15,23 @@ const phoneSchema = z
     "Wrong phone format"
   );
 const tokenSchema = z.coerce.number().min(100000).max(999999);
+
+async function getToken() {
+  const token = crypto.randomInt(100000, 999999).toString();
+
+  const exists = await db.sMSToken.findUnique({
+    where: {
+      token,
+    },
+    select: { id: true },
+  });
+
+  if (exists) {
+    return getToken();
+  }
+
+  return token;
+}
 
 interface ActionState {
   token: boolean;
@@ -22,9 +44,40 @@ export async function smsLogin(prevState: ActionState, formData: FormData) {
   if (!prevState.token) {
     const result = phoneSchema.safeParse(phone);
 
-    if (!result.success) {
+    const { success, data: phoneNumber } = result;
+
+    if (!success) {
       return { token: false, error: result.error.flatten() };
     }
+
+    await db.sMSToken.deleteMany({
+      where: {
+        user: {
+          phone: phoneNumber,
+        },
+      },
+    });
+
+    const token = await getToken();
+
+    await db.sMSToken.create({
+      data: {
+        token,
+        user: {
+          connectOrCreate: {
+            where: {
+              phone: phoneNumber,
+            },
+            create: {
+              username: crypto.randomBytes(10).toString("hex"),
+              phone: phoneNumber,
+            },
+          },
+        },
+      },
+    });
+
+    // send the token using twlio
 
     return {
       token: true,
