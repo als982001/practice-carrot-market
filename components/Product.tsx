f1162e625cba00592e298bd974479b56a7a3844b
jaemin <als982001@naver.com>
2024-11-24 14:32:47 +0900

    상품 정보 모달 띄워줄 버튼 추가

--- components/Product.tsx
+++ components/Product.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import Link from "next/link";
 
 import { formatToTimeAgo, formatToWon } from "@/lib/utils";
+import React from "react";
 
 interface IProps {
   title: string;
@@ -19,17 +20,24 @@ export default function Product({
   photo,
   id,
 }: IProps) {
+  const handleButtonClick = (e: React.MouseEvent) => {
+    e.preventDefault();
+  };
+
   return (
     <Link href={`products/${id}`} className="flex gap-5">
       <div className="relative size-28 rounded-md overflow-hidden">
         <Image fill src={photo} className="object-cover" alt={title} />
       </div>
-      <div className="flex flex-col gap-1 *:text-white">
-        <span className="text-lg">{title}</span>
-        <span className="text-sm text-neutral-500">
-          {formatToTimeAgo(created_at.toString())}
-        </span>
-        <span className="text-lg font-semibold">{formatToWon(price)}</span>
+      <div className="w-full flex justify-between items-center">
+        <div className="flex flex-col gap-1 *:text-white">
+          <span className="text-lg">{title}</span>
+          <span className="text-sm text-neutral-500">
+            {formatToTimeAgo(created_at.toString())}
+          </span>
+          <span className="text-l4g font-semibold">{formatToWon(price)}</span>
+        </div>
+        <button onClick={handleButtonClick}>요약해서 보기</button>
       </div>
     </Link>
   );
