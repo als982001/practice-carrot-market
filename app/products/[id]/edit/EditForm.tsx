8f617d953b7aa234a588f849b7cd0ccda23fc652
jaemin <als982001@naver.com>
2025-02-08 16:39:00 +0900

    상품 수정할 때 기본 정보 input에 보이게 수정

--- app/products/[id]/edit/EditForm.tsx
+++ app/products/[id]/edit/EditForm.tsx
@@ -4,7 +4,7 @@ import { useFormState } from "react-dom";
 
 import { PhotoIcon } from "@heroicons/react/24/solid";
 
-import { uploadProduct } from "../../add/actions";
+import { updateProduct } from "../../add/actions";
 import { useState } from "react";
 import Input from "@/components/Input";
 import Button from "@/components/Button";
@@ -29,8 +29,21 @@ interface IProps {
 }
 
 export default function EditForm({ product }: IProps) {
-  const [preview, setPreview] = useState("");
-  const [state, action] = useFormState(uploadProduct, null);
+  const { title, price, description, id: productId } = product;
+
+  const [editedProduct, setEditedProduct] = useState<{
+    title: string;
+    price: number;
+    description: string;
+  }>({
+    title: title,
+    price: price,
+    description: description,
+  });
+
+  const [selectedFile, setSelectedFile] = useState<File | null>(null);
+  const [preview, setPreview] = useState(product.photo);
+  const [state, action] = useFormState(updateProduct, null);
 
   const onImageChange = async (event: React.ChangeEvent<HTMLInputElement>) => {
     const {
@@ -51,7 +64,21 @@ export default function EditForm({ product }: IProps) {
 
     const url = URL.createObjectURL(file);
 
+    setSelectedFile(file);
     setPreview(url);
+  };
+
+  const onInputChange = (
+    event: React.ChangeEvent<HTMLInputElement>,
+    key: "title" | "price" | "description"
+  ) => {
+    const newValue =
+      key === "price" ? Number(event.target.value) : event.target.value;
+
+    setEditedProduct((prev) => ({
+      ...prev,
+      [key]: newValue,
+    }));
   };
 
   return (
@@ -61,7 +88,7 @@ export default function EditForm({ product }: IProps) {
           htmlFor="photo"
           className="border-2 aspect-square flex items-center justify-center flex-col text-neutral-300 border-neutral-300 rounded-md border-dashed cursor-pointer bg-center bg-cover"
           style={{
-            backgroundImage: `url(${product.photo})`,
+            backgroundImage: `url(${preview})`,
           }}
         >
           {preview === "" && (
@@ -82,11 +109,17 @@ export default function EditForm({ product }: IProps) {
           onChange={onImageChange}
           className="hidden"
         />
+        {!selectedFile && (
+          <input type="hidden" name="existingPhoto" value={product.photo} />
+        )}
+        <input type="hidden" name="productId" value={productId} />
         <Input
           required
           placeholder="제목"
           type="text"
           name="title"
+          value={editedProduct.title}
+          onChange={(event) => onInputChange(event, "title")}
           errors={state?.fieldErrors.title}
         />
         <Input
@@ -94,6 +127,8 @@ export default function EditForm({ product }: IProps) {
           placeholder="가격"
           type="number"
           name="price"
+          value={editedProduct.price}
+          onChange={(event) => onInputChange(event, "price")}
           errors={state?.fieldErrors.price}
         />
         <Input
@@ -101,6 +136,8 @@ export default function EditForm({ product }: IProps) {
           placeholder="자세한 설명"
           type="text"
           name="description"
+          value={editedProduct.description}
+          onChange={(event) => onInputChange(event, "description")}
           errors={state?.fieldErrors.description}
         />
         <Button text="작성 완료" />
